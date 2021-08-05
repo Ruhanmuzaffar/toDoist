7d06845bd9a335304ff0ae54a2c3e01fc7843fc6
Ruhanmuzaffar <31547226+Ruhanmuzaffar@users.noreply.github.com>
2021-08-05 18:31:13 +0530

    moved task functions to utils file

--- routes/api/TasksDocs.js
+++ routes/api/TasksDocs.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const queries = require("../../util/queries/queries");
+const utils = require("../../util/util");
 // prisma client
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
@@ -17,126 +18,21 @@ app.use(express.urlencoded({ extended: false }));
 
 // get all tasks
 
-app.get("/tasks", getAllTasks);
-async function getAllTasks(req, res) {
-  try {
-    const allTasks = await queries.findAllTasks();
-    res.json(allTasks);
-  } catch (error) {
-    console.log(error);
-  }
-}
+app.get("/tasks", utils.getAllTasks);
 
 // get single task
 
-app.get("/tasks/:id", getSingleTask);
-
-async function getSingleTask(req, res) {
-  const taskId = req.params.id;
-
-  try {
-    const singleTask = await queries.findTaskById(taskId);
-
-    if (singleTask) {
-      res.json(singleTask);
-    } else {
-      res.status(400).json({ msg: "task does not exist" });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
+app.get("/tasks/:id", utils.getSingleTask);
 
 // create task
 
-app.post("/tasks", createTask);
-
-async function createTask(req, res) {
-  // handle foreign key constraint
-
-  const { content, description, project_id } = req.body;
-  console.log("body>>", req.body);
-
-  if (!content) {
-    return res.status(400).json({
-      msg: "Please add task content",
-    });
-  }
-  if (!project_id) {
-    return res.status(400).json({
-      msg: "Please add project to which to add task ",
-    });
-  }
-
-  try {
-    const project = await prisma.project.findUnique({
-      where: {
-        id: project_id,
-      },
-    });
-
-    if (project) {
-      // put into tasks with project id from projectId
-
-      const task = await queries.createTask(content, description, project_id);
-      res.json({ msg: "task created sucessfully", task });
-    } else {
-      res.json({ msg: "Project does not exist" });
-    }
-  } catch (error) {}
-}
+app.post("/tasks", utils.createTask);
 
 // delete task
-app.delete("/tasks/:id", deteteTask);
-
-async function deteteTask(req, res) {
-  const taskId = req.params.id;
-
-  try {
-    const singleTask = await queries.findTaskById(taskId);
-
-    if (singleTask) {
-      //delete task
-      await queries.deleteTask(taskId);
-      res.json({ msg: "task deleted sucessfully" });
-    } else {
-      res.status(400).json({ msg: "task does not exist" });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
+app.delete("/tasks/:id", utils.deleteTask);
 
 // update task
 
-app.put("/tasks/:id", editTask);
-
-async function editTask(req, res) {
-  const taskId = parseInt(req.params.id);
-
-  // check if it exists
-
-  const { content, description } = req.body;
-
-  if (!content) {
-    return res.json({
-      msg: "please enter the task content ",
-    });
-  }
-
-  try {
-    const task = await queries.findTaskById(taskId);
-
-    if (task) {
-      // update task
-      await queries.editTask(task,taskId,content,description)
-      res.json({ msg: "updated sucessfully" });
-    } else {
-      res.status(404).json({ msg: `task with id: ${taskId} does not exist` });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
+app.put("/tasks/:id", utils.editTask);
 
 module.exports = app;
--- util/util.js
+++ util/util.js
@@ -97,7 +97,117 @@ async function deleteProject(req, res) {
   }
 }
 
+// get all tasks
 
+async function getAllTasks(req, res) {
+  try {
+    const allTasks = await queries.findAllTasks();
+    res.json(allTasks);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// get single task
+
+async function getSingleTask(req, res) {
+  const taskId = req.params.id;
+
+  try {
+    const singleTask = await queries.findTaskById(taskId);
+
+    if (singleTask) {
+      res.json(singleTask);
+    } else {
+      res.status(400).json({ msg: "task does not exist" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// create task
+
+async function createTask(req, res) {
+  // handle foreign key constraint
+
+  const { content, description, project_id } = req.body;
+  console.log("body>>", req.body);
+
+  if (!content) {
+    return res.status(400).json({
+      msg: "Please add task content",
+    });
+  }
+  if (!project_id) {
+    return res.status(400).json({
+      msg: "Please add project to which to add task ",
+    });
+  }
+
+  try {
+    const project = await queries.findProjectById(project_id);
+
+    if (project) {
+      // put into tasks with project id from projectId
+
+      const task = await queries.createTask(content, description, project_id);
+      res.json({ msg: "task created sucessfully", task });
+    } else {
+      res.json({ msg: "Project does not exist" });
+    }
+  } catch (error) {}
+}
+
+// delete task
+
+async function deleteTask(req, res) {
+  const taskId = req.params.id;
+
+  try {
+    const singleTask = await queries.findTaskById(taskId);
+
+    if (singleTask) {
+      //delete task
+      await queries.deleteTask(taskId);
+      res.json({ msg: "task deleted sucessfully" });
+    } else {
+      res.status(400).json({ msg: "task does not exist" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// update task
+
+async function editTask(req, res) {
+  const taskId = parseInt(req.params.id);
+
+  // check if it exists
+
+  const { content, description } = req.body;
+
+  if (!content) {
+    return res.json({
+      msg: "please enter the task content ",
+    });
+  }
+
+  try {
+    const task = await queries.findTaskById(taskId);
+
+    if (task) {
+      // update task
+      await queries.editTask(task, taskId, content, description);
+      res.json({ msg: "updated sucessfully" });
+    } else {
+      res.status(404).json({ msg: `task with id: ${taskId} does not exist` });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = {
   getProjects,
@@ -105,4 +215,9 @@ module.exports = {
   createProject,
   editProject,
   deleteProject,
+  getAllTasks,
+  getSingleTask,
+  createTask,
+  deleteTask,
+  editTask,
 };
